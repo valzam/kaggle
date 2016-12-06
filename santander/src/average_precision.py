64e6a57495be12211d3ac074b777649f488071a2
Valentin Zambelli <valentin.zambelli@gmail.com>
2016-12-06 13:23:47 +0100

    fixed issues with conversion to names

--- santander/src/average_precision.py
+++ santander/src/average_precision.py
@@ -28,14 +28,14 @@ def sort_predictions_with_names(preds, names):
     return preds_sorted, names_sorted
 
 def convert_to_names(arr, treshhold=1.0):
-    y_cols = ['ind_ahor_fin_ult1', 'ind_aval_fin_ult1', 'ind_cco_fin_ult1',
-              'ind_cder_fin_ult1', 'ind_cno_fin_ult1', 'ind_ctju_fin_ult1',
-              'ind_ctma_fin_ult1', 'ind_ctop_fin_ult1', 'ind_ctpp_fin_ult1',
-              'ind_deco_fin_ult1', 'ind_deme_fin_ult1', 'ind_dela_fin_ult1',
-              'ind_ecue_fin_ult1', 'ind_fond_fin_ult1', 'ind_hip_fin_ult1',
+    y_cols = ['ind_cco_fin_ult1', 'ind_cder_fin_ult1', 'ind_cno_fin_ult1',
+              'ind_ctju_fin_ult1', 'ind_ctma_fin_ult1', 'ind_ctop_fin_ult1',
+              'ind_ctpp_fin_ult1', 'ind_deco_fin_ult1', 'ind_dela_fin_ult1',
+              'ind_deme_fin_ult1', 'ind_ecue_fin_ult1', 'ind_fond_fin_ult1',
+              'ind_hip_fin_ult1', 'ind_nom_pens_ult1', 'ind_nomina_ult1',
               'ind_plan_fin_ult1', 'ind_pres_fin_ult1', 'ind_reca_fin_ult1',
-              'ind_tjcr_fin_ult1', 'ind_valo_fin_ult1', 'ind_viv_fin_ult1',
-              'ind_nomina_ult1', 'ind_nom_pens_ult1', 'ind_recibo_ult1']
+              'ind_recibo_ult1', 'ind_tjcr_fin_ult1', 'ind_valo_fin_ult1',
+              'ind_viv_fin_ult1']
 
     preds_sorted, names_sorted = sort_predictions_with_names(arr, y_cols)
     predicted_additions = []
--- santander/src/create_multiclass.py
+++ santander/src/create_multiclass.py
@@ -6,11 +6,20 @@ def create_multiclass_dataset(X_train, y_train):
     y_train["ncodpers"] = X_train["ncodpers"]
     y_train = y_train.set_index("ncodpers")
 
+    # y_train is a dataframe with 24 columns
+    # Each column value indicates whether the product was added in 2015-06,
+    # in comparison to 2015-05
+
+    # Create a multi-index, based on ncodpers and each columns
+    # Reindex to create a "long" dataframe (one row for each (product, ncodpers) tuple
     stacked = y_train.stack()
     filtered_y_train = stacked.reset_index()
     filtered_y_train.columns = ["ncodpers", "product", "newly_added"]
 
+    # Only take the rows where a product was added
     filtered_y_train = filtered_y_train[filtered_y_train["newly_added"] == 1]
+
+    # Merge with the training set
     multiclass_training = filtered_y_train.merge(X_train, on="ncodpers", how="left")
 
     # Create categories as integers
@@ -20,6 +29,7 @@ def create_multiclass_dataset(X_train, y_train):
         pickle.dump(le, handler)
 
     multiclass_training["target"] = le.transform(multiclass_training["product"])
+    multiclass_training.drop("newly_added", inplace=True, axis=1)
 
     return multiclass_training
 
